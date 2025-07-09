9b390be5f4d5b5c5b9eef8cfddff8cbee298e24d
Christian Koch <ckochx@gmail.com>
2025-07-09 09:12:54 -0500

    deps: remove mix.exs deps fn

--- mix.exs
+++ mix.exs
@@ -4,26 +4,30 @@ defmodule ElixirNoDeps.MixProject do
   def project do
     [
       app: :elixir_no_deps,
+      config_path: "config/config.exs",
+      # elixirc_paths: elixirc_paths(Mix.env()),
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      # no deps
+      # deps: deps()
+    ]
+  end
+
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :ssh],
+      mod: []
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :ssh],
       mod: {ElixirNoDeps.Application, []}
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
