5e8818e661a44e2d2eba73df58b403d9c4e11519
Christian Koch <ckochx@gmail.com>
2025-07-09 08:43:04 -0500

    initial commit- ElixirConf here we come

---
+++ lib/elixir_no_deps.ex
@@ -0,0 +1,18 @@
+defmodule ElixirNoDeps do
+  @moduledoc """
+  Documentation for `ElixirNoDeps`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirNoDeps.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
